53f2cc7c69860bc3075780adbfe4e90c33a6ac9a
leoxanigm <leoxanigm@gmail.com>
2022-10-21 20:15:17 +0200

    add cross policy header

--- dist/app.js
+++ dist/app.js
@@ -7,11 +7,13 @@ const express_1 = __importDefault(require("express"));
 const bookNames_1 = __importDefault(require("./bookNames"));
 const app = (0, express_1.default)();
 app.get('/all', (req, res) => {
+    /* Add cross policy header */
     res.header('Access-Control-Allow-Origin', '*');
     res.json(bookNames_1.default);
 });
 app.get('/search', (req, res) => {
     const foundBooks = bookNames_1.default.filter(book => { var _a; return book.toLowerCase().includes((_a = req.query) === null || _a === void 0 ? void 0 : _a.text.toLowerCase()); });
+    res.header('Access-Control-Allow-Origin', '*');
     res.json(foundBooks);
 });
 app.listen(8081, () => {
