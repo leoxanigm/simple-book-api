6ec4fddceb20d577049ef3a90504bad69a19fedd
leoxanigm <leoxanigm@gmail.com>
2022-10-21 19:14:32 +0200

    add /all and /search API ends

--- app.ts
+++ app.ts
@@ -1,3 +1,19 @@
-const express = require('express');
+import express from 'express';
+import bookNames from './bookNames';
 
+const app = express();
 
+app.get('/all', (req, res) => {
+  res.json(bookNames);
+});
+
+app.get('/search', (req: any, res) => {
+  const foundBooks = bookNames.filter(book =>
+    book.toLowerCase().includes(req.query?.text.toLowerCase())
+  );
+  res.json(foundBooks);
+});
+
+app.listen(8081, () => {
+  console.log('Simple API running on port 8081...');
+});
---
+++ dist/app.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const bookNames_1 = __importDefault(require("./bookNames"));
+const app = (0, express_1.default)();
+app.get('/all', (req, res) => {
+    res.json(bookNames_1.default);
+});
+app.get('/search', (req, res) => {
+    const foundBooks = bookNames_1.default.filter(book => { var _a; return book.toLowerCase().includes((_a = req.query) === null || _a === void 0 ? void 0 : _a.text.toLowerCase()); });
+    res.json(foundBooks);
+});
+app.listen(8081, () => {
+    console.log('Simple API running on port 8081...');
+});
